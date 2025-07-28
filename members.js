58b1bcf349b9c6a3753c403cfbcf212730bb3f05
조아 <parkjoah@naver.com>
2025-07-28 10:47:28 +0900

    feat: 멤버소개 멤버 리스트

---
+++ members.js
@@ -0,0 +1,30 @@
+const members = [
+  {
+    name: "변경탁 TL",
+    prfImg: "../img/member_1.png",
+  },
+  {
+    name: "박조아",
+    prfImg: "../img/member_2.png",
+  },
+  {
+    name: "손민재",
+    prfImg: "../img/member_3.png",
+  },
+  {
+    name: "이경민",
+    prfImg: "../img/member_4.png",
+  },
+  {
+    name: "이수림",
+    prfImg: "../img/member_5.png",
+  },
+  {
+    name: "유지우",
+    prfImg: "../img/member_6.png",
+  },
+  {
+    name: "유창민",
+    prfImg: "../img/member_7.png",
+  },
+];
